36fe36ef15b822b7f351796eb6b1b02b7b48b330
Alexander Zobnin <alexanderzobnin@gmail.com>
2018-10-30 21:48:39 +0300

    db connector: fix tests

--- src/datasource-zabbix/specs/dbConnector.test.js
+++ src/datasource-zabbix/specs/dbConnector.test.js
@@ -20,18 +20,18 @@ describe('DBConnector', () => {
       };
     });
 
-    it('should load datasource by name by default', () => {
+    it('should try to load datasource by name first', () => {
       ctx.options = {
         datasourceName: 'bar'
       };
-      const dbConnector = new DBConnector(ctx.options, backendSrvMock, datasourceSrvMock);
+      const dbConnector = new DBConnector(ctx.options, datasourceSrvMock);
       dbConnector.loadDBDataSource();
       expect(datasourceSrvMock.getAll).not.toHaveBeenCalled();
       expect(datasourceSrvMock.loadDatasource).toHaveBeenCalledWith('bar');
     });
 
     it('should load datasource by id if name not present', () => {
-      const dbConnector = new DBConnector(ctx.options, backendSrvMock, datasourceSrvMock);
+      const dbConnector = new DBConnector(ctx.options, datasourceSrvMock);
       dbConnector.loadDBDataSource();
       expect(datasourceSrvMock.getAll).toHaveBeenCalled();
       expect(datasourceSrvMock.loadDatasource).toHaveBeenCalledWith('foo');
@@ -39,14 +39,14 @@ describe('DBConnector', () => {
 
     it('should throw error if no name and id specified', () => {
       ctx.options = {};
-      const dbConnector = new DBConnector(ctx.options, backendSrvMock, datasourceSrvMock);
-      return expect(dbConnector.loadDBDataSource()).rejects.toBe('SQL Data Source name should be specified');
+      const dbConnector = new DBConnector(ctx.options, datasourceSrvMock);
+      return expect(dbConnector.loadDBDataSource()).rejects.toBe('Data Source name should be specified');
     });
 
     it('should throw error if datasource with given id is not found', () => {
       ctx.options.datasourceId = 45;
-      const dbConnector = new DBConnector(ctx.options, backendSrvMock, datasourceSrvMock);
-      return expect(dbConnector.loadDBDataSource()).rejects.toBe('SQL Data Source with ID 45 not found');
+      const dbConnector = new DBConnector(ctx.options, datasourceSrvMock);
+      return expect(dbConnector.loadDBDataSource()).rejects.toBe('Data Source with ID 45 not found');
     });
   });
 });
